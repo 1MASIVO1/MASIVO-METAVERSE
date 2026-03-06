7a28fee3adf26671eda2df7d9ce3671bfdb776cc
MASIVO <1masivo1@gmail.com>
2026-03-06 17:29:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,35 +2,32 @@ const SUPABASE_URL="https://rnkuxwsuztewgbdmjyxt.supabase.co"
 
 const SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJua3V4d3N1enRld2diZG1qeXh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5ODU4MjQsImV4cCI6MjA4NzU2MTgyNH0.mwGzWUk6xOry9BcwqwRnXGFfGMwoetg6D2pxAz7_eN4"
 
-const supabaseClient=supabase.createClient(SUPABASE_URL,SUPABASE_KEY)
+const supabaseClient = supabase.createClient(SUPABASE_URL,SUPABASE_KEY)
 
 const nftNames=[
-"void-assassin",
-"dragon-armor",
-"storm-warden",
-"shadow-ronin",
-"iron-titan",
-"cyber-samurai",
-"phoenix-guardian",
-"dark-knight",
-"neon-hunter"
+
+1,2,3,4,5,6,7,8,9
+
 ]
 
 const grid=document.getElementById("nft-grid")
 
-nftNames.forEach((name,i)=>{
+nftNames.forEach((n,i)=>{
 
-let div=document.createElement("div")
-div.className="nft"
+let card=document.createElement("div")
 
-div.innerHTML=`
+card.className="nft"
 
-<img src="images/${i+1}.png" onclick="viewNFT(${i})">
+card.innerHTML=`
+
+<img src="images/${n}.png" onclick="viewNFT(${i})">
 
 <div class="buttons">
 
 <button onclick="likeNFT(${i})">LIKE</button>
+
 <button onclick="downloadNFT(${i})">DOWNLOAD</button>
+
 <button onclick="shareNFT(${i})">SHARE</button>
 
 </div>
@@ -38,15 +35,18 @@ div.innerHTML=`
 <div class="counter">
 
 👁 <span id="views${i}">0</span>
+
 ❤️ <span id="likes${i}">0</span>
+
 ⬇ <span id="downloads${i}">0</span>
+
 🔗 <span id="shares${i}">0</span>
 
 </div>
 
 `
 
-grid.appendChild(div)
+grid.appendChild(card)
 
 loadStats(i)
 
@@ -55,12 +55,16 @@ loadStats(i)
 async function loadStats(id){
 
 let {data}=await supabaseClient
+
 .from("nft_stats")
+
 .select("*")
+
 .eq("id",id)
+
 .single()
 
-if(!data)return
+if(!data) return
 
 document.getElementById("views"+id).innerText=data.views
 document.getElementById("likes"+id).innerText=data.likes
@@ -72,16 +76,23 @@ document.getElementById("shares"+id).innerText=data.shares
 async function updateStat(id,field){
 
 let {data}=await supabaseClient
+
 .from("nft_stats")
-select("*")
+
+.select("*")
+
 .eq("id",id)
+
 .single()
 
 let value=data[field]+1
 
 await supabaseClient
+
 .from("nft_stats")
+
 .update({[field]:value})
+
 .eq("id",id)
 
 document.getElementById(field+id).innerText=value
@@ -89,26 +100,33 @@ document.getElementById(field+id).innerText=value
 }
 
 function viewNFT(id){
+
 updateStat(id,"views")
+
 }
 
 function likeNFT(id){
+
 updateStat(id,"likes")
+
 }
 
 function downloadNFT(id){
+
 updateStat(id,"downloads")
+
 window.open(`images/${id+1}.png`)
+
 }
 
 function shareNFT(id){
 
 updateStat(id,"shares")
 
-let url=window.location.href+"?nft="+id
+let link=window.location.href+"?nft="+id
 
-navigator.clipboard.writeText(url)
+navigator.clipboard.writeText(link)
 
-alert("NFT link copied!")
+alert("Link copied")
 
 }
