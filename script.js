e20ed1fba6aaad4f35db409a69263063ed55d41d
MASIVO <1masivo1@gmail.com>
2026-03-07 15:01:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,83 @@
-/* TODO TU CODIGO ORIGINAL */
+/* ===================== */
+/* CONEXION SUPABASE */
+/* ===================== */
 
-/* RANKING REAL */
+const SUPABASE_URL=https://rnkuxwsuztewgbdmjyxt.supabase.co
+const SUPABASE_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJua3V4d3N1enRld2diZG1qeXh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5ODU4MjQsImV4cCI6MjA4NzU2MTgyNH0.mwGzWUk6xOry9BcwqwRnXGFfGMwoetg6D2pxAz7_eN4
 
-async function ordenarNFT(){
+const supabaseClient=supabase.createClient(SUPABASE_URL,SUPABASE_KEY)
 
-let {data}=await supabase.from("stats").select("*")
 
-let likes=data.filter(x=>x.tipo==="like").length
+
+/* ===================== */
+/* LIKE GLOBAL INTERNET */
+/* ===================== */
+
+async function like(btn){
+
+let nft=btn.closest(".nft")
+let id=nft.getAttribute("data-id")
+
+let el=nft.querySelector(".likes")
+
+let num=parseInt(el.innerText.replace(/\D/g,''))
+
+num++
+
+el.innerText="❤️ "+num
+
+await supabaseClient
+.from("likes")
+.insert([{nft:id}])
+
+}
+
+
+
+/* ===================== */
+/* RANKING MUNDIAL */
+/* ===================== */
+
+async function rankingGlobal(){
+
+let {data}=await supabaseClient
+.from("likes")
+.select("*")
+
+let conteo={}
+
+data.forEach(row=>{
+
+if(!conteo[row.nft]) conteo[row.nft]=0
+
+conteo[row.nft]++
+
+})
 
 let grid=document.querySelector(".grid")
 
 let items=[...document.querySelectorAll(".nft")]
 
-items.sort(()=>Math.random()-0.5 + likes)
+items.sort((a,b)=>{
+
+let idA=a.getAttribute("data-id")
+let idB=b.getAttribute("data-id")
+
+return (conteo[idB]||0)-(conteo[idA]||0)
+
+})
 
 items.forEach(el=>grid.appendChild(el))
 
 }
 
-setInterval(ordenarNFT,5000)
+setInterval(rankingGlobal,4000)
 
 
 
-/* SISTEMA DE LOGROS */
+/* ===================== */
+/* SISTEMA LOGROS */
+/* ===================== */
 
 function actualizarLogros(){
 
@@ -58,25 +113,9 @@ setInterval(actualizarLogros,2000)
 
 
 
-/* ================= */
-/* FUNCIONES BOTONES */
-/* ================= */
-
-function like(btn){
-
-let nft=btn.closest(".nft")
-
-let el=nft.querySelector(".likes")
-
-let num=parseInt(el.innerText.replace(/\D/g,''))
-
-num++
-
-el.innerText="❤️ "+num
-
-}
-
-
+/* ===================== */
+/* BOTONES NORMALES */
+/* ===================== */
 
 function vista(){
 
