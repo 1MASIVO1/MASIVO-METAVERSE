97ca6a3f3b553b0e73c4a79693e2a12d96e6e2eb
MASIVO <1masivo1@gmail.com>
2026-03-06 20:14:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,71 @@
-const likes = document.querySelectorAll(".like")
+const supabaseUrl = "https://rnkuxwsuztewgbdmjyxt.supabase.co";
 
-likes.forEach((btn)=>{
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJua3V4d3N1enRld2diZG1qeXh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5ODU4MjQsImV4cCI6MjA4NzU2MTgyNH0.mwGzWUk6xOry9BcwqwRnXGFfGMwoetg6D2pxAz7_eN4";
 
-btn.onclick = ()=>{
+const client = supabase.createClient(supabaseUrl,supabaseKey);
 
-let count = btn.nextElementSibling
+const container=document.getElementById("nftContainer");
 
-count.innerText = parseInt(count.innerText)+1
+const NFTs=[
+"1.png",
+"2.png",
+"3.png",
+"4.png",
+"5.png",
+"6.png",
+"7.png",
+"8.png",
+"9.png"
+];
 
+NFTs.forEach(nft=>{
+
+const card=document.createElement("div");
+card.className="nft";
+
+card.innerHTML=`
+
+<img src="imagenes/${nft}">
+
+<div class="botones">
+
+<button onclick="likeNFT('${nft}')">❤️</button>
+
+<button onclick="viewNFT('${nft}')">👁️</button>
+
+<button onclick="downloadNFT('${nft}')">⬇️</button>
+
+<button onclick="shareNFT('${nft}')">🔗</button>
+
+</div>
+
+`;
+
+container.appendChild(card);
+
+});
+
+async function likeNFT(id){
+await client.from("nfts").update({likes:1}).eq("id",id);
 }
 
-})
+async function viewNFT(id){
+await client.from("nfts").update({views:1}).eq("id",id);
+}
+
+async function downloadNFT(id){
+window.open("imagenes/"+id);
+await client.from("nfts").update({downloads:1}).eq("id",id);
+}
+
+async function shareNFT(id){
+
+const url=window.location.href+"#"+id;
+
+navigator.clipboard.writeText(url);
+
+await client.from("nfts").update({shares:1}).eq("id",id);
+
+alert("Link copiado");
+
+}
