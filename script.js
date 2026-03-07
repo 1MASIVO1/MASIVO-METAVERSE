93cfbb6a0029064c8433e76f76e5f9096a2eac66
MASIVO <1masivo1@gmail.com>
2026-03-06 23:16:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,58 @@
-const supabase = window.supabase.createClient(
-"https://rnkuxwsuztewgbdmjyxt.supabase.co",
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJua3V4d3N1enRld2diZG1qeXh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5ODU4MjQsImV4cCI6MjA4NzU2MTgyNH0.mwGzWUk6xOry9BcwqwRnXGFfGMwoetg6D2pxAz7_eN4"
-)
+function abrirNFT(img){
 
-const gallery = document.getElementById("gallery")
-const viewer = document.getElementById("viewer")
-const viewerImg = document.getElementById("viewer-img")
-
-const TOTAL_NFT = 9
-
-function createCard(id){
-
-const img = `images/nft${id}.png`
-
-const card = document.createElement("div")
-card.className="card"
-
-card.innerHTML=`<img src="${img}">`
-
-gallery.appendChild(card)
-
-card.onclick=()=>{
-
-viewer.style.display="flex"
-viewerImg.src=img
+document.getElementById("modal").style.display="flex"
+document.getElementById("nftGrande").src=img.src
 
 }
 
-}
+function cerrarNFT(){
 
-for(let i=1;i<=TOTAL_NFT;i++){
-
-createCard(i)
+document.getElementById("modal").style.display="none"
 
 }
 
-viewer.onclick=()=>viewer.style.display="none"
+function like(btn){
+
+let count=localStorage.getItem("likes") || 0
+count++
+localStorage.setItem("likes",count)
+
+}
+
+function vista(){
+
+let count=localStorage.getItem("vistas") || 0
+count++
+localStorage.setItem("vistas",count)
+
+}
+
+function descargar(){
+
+let count=localStorage.getItem("descargas") || 0
+count++
+localStorage.setItem("descargas",count)
+
+}
+
+function share(){
+
+let count=localStorage.getItem("shares") || 0
+count++
+localStorage.setItem("shares",count)
+
+}
+
+function ranking(){
+
+let grid=document.querySelector(".grid")
+
+let items=[...document.querySelectorAll(".nft")]
+
+items.sort(()=>Math.random()-0.5)
+
+items.forEach(el=>grid.appendChild(el))
+
+}
+
+setInterval(ranking,5000)
