4159b5e6abfdd1fd00a6847ed476fcbb657c1e30
MASIVO <1masivo1@gmail.com>
2026-03-07 00:21:49 -0500

    Update fireworks.js

--- fireworks.js
+++ fireworks.js
@@ -1,26 +1,33 @@
-const canvas = document.getElementById("logoFireworks")
+const canvas = document.getElementById("fireworks")
 const ctx = canvas.getContext("2d")
 
-canvas.width = window.innerWidth
-canvas.height = 150
+const titulo = document.getElementById("tituloLogo")
+
+const rect = titulo.getBoundingClientRect()
+
+canvas.width = rect.width
+canvas.height = rect.height + 20
+
+canvas.style.width = rect.width + "px"
+canvas.style.height = rect.height + "px"
 
 let particles=[]
 
-function firework(){
+function createFirework(){
 
 let x=Math.random()*canvas.width
-let y=Math.random()*120
+let y=Math.random()*canvas.height/2
 
-let colors=["#ff0040","#00ffff","#ffcc00","#00ff6a","#ff00ff"]
+let colors=["#00ffff","#ff00ff","#00ff88","#ffcc00","#ff3355"]
 
-for(let i=0;i<30;i++){
+for(let i=0;i<25;i++){
 
 particles.push({
 
 x:x,
 y:y,
-dx:(Math.random()-0.5)*6,
-dy:(Math.random()-0.5)*6,
+dx:(Math.random()-0.5)*5,
+dy:(Math.random()-0.5)*5,
 life:80,
 color:colors[Math.floor(Math.random()*colors.length)]
 
@@ -46,7 +53,9 @@ ctx.fillStyle=p.color
 ctx.fill()
 
 if(p.life<=0){
+
 particles.splice(i,1)
+
 }
 
 })
@@ -55,6 +64,6 @@ requestAnimationFrame(animate)
 
 }
 
-setInterval(firework,1500)
+setInterval(createFirework,1200)
 
 animate()
