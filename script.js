28bf13f23ea165a2d4875c86a6e3bfe9bd6e9a3f
MASIVO <1masivo1@gmail.com>
2026-03-06 23:11:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,58 +4,35 @@ const supabase = window.supabase.createClient(
 )
 
 const gallery = document.getElementById("gallery")
+const viewer = document.getElementById("viewer")
+const viewerImg = document.getElementById("viewer-img")
 
 const TOTAL_NFT = 9
 
 function createCard(id){
 
-const imgPath = `images/nft${id}.png`
+const img = `images/nft${id}.png`
 
 const card = document.createElement("div")
 card.className="card"
 
-const img = document.createElement("img")
-
-img.src = imgPath
-
-img.onerror = function(){
-console.log("imagen no encontrada:", imgPath)
-}
-
-img.onclick = ()=>openViewer(id,imgPath)
-
-card.appendChild(img)
+card.innerHTML=`<img src="${img}">`
 
 gallery.appendChild(card)
 
-}
-
-async function openViewer(id,img){
-
-const viewer=document.getElementById("viewer")
-const viewerImg=document.getElementById("viewer-img")
+card.onclick=()=>{
 
 viewer.style.display="flex"
-
 viewerImg.src=img
 
-let {data}=await supabase
-.from("nfts")
-.select("*")
-.eq("id",id)
-.single()
-
-if(!data) return
-
-let views=data.views+1
-
-await supabase
-.from("nfts")
-.update({views:views})
-.eq("id",id)
+}
 
 }
 
 for(let i=1;i<=TOTAL_NFT;i++){
+
 createCard(i)
+
 }
+
+viewer.onclick=()=>viewer.style.display="none"
