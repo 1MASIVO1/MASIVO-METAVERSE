01d220af023c01ac0138e8b55f67ff9636df228c
MASIVO <1masivo1@gmail.com>
2026-03-07 14:13:49 -0500

    Update fireworks.js

--- fireworks.js
+++ fireworks.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("fireworks")
 const ctx = canvas.getContext("2d")
 
-const titulo = document.querySelector(".titulo")
+const titulo = document.getElementById("tituloLogo")
 
 const rect = titulo.getBoundingClientRect()
 
