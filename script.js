383eeb2eef8cd5ef694bc53fc2c2990f11cfd995
MASIVO <1masivo1@gmail.com>
2026-03-07 18:35:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 const supabaseUrl = "https://rnkuxwsuztewgbdmjyxt.supabase.co"
 
-const supabaseKey = "TU_ANON_PUBLIC_KEY"
+const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJua3V4d3N1enRld2diZG1qeXh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5ODU4MjQsImV4cCI6MjA4NzU2MTgyNH0.mwGzWUk6xOry9BcwqwRnXGFfGMwoetg6D2pxAz7_eN4
 
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
 
