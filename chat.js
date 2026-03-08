557cd2977f1568d3d1b530d2d311d26938c50fed
MASIVO <1masivo1@gmail.com>
2026-03-07 21:54:29 -0500

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,1 +1,37 @@
+async function enviarMensaje(){
 
+let input=document.getElementById("chatInput")
+
+let texto=input.value
+
+await supabase
+
+.from("chat")
+
+.insert({mensaje:texto})
+
+input.value=""
+
+}
+
+async function cargarChat(){
+
+const {data}=await supabase
+
+.from("chat")
+
+.select("*")
+
+const div=document.getElementById("mensajes")
+
+div.innerHTML=""
+
+data.forEach(m=>{
+
+div.innerHTML+=`<div>${m.mensaje}</div>`
+
+})
+
+}
+
+setInterval(cargarChat,2000)
