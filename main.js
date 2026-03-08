0cbac7be587d120545af954cdb11e18a29345faa
MASIVO <1masivo1@gmail.com>
2026-03-07 22:27:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js"
 
+const supabase = createClient(
+https://rnkuxwsuztewgbdmjyxt.supabase.co
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJua3V4d3N1enRld2diZG1qeXh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5ODU4MjQsImV4cCI6MjA4NzU2MTgyNH0.mwGzWUk6xOry9BcwqwRnXGFfGMwoetg6D2pxAz7_eN4
+)
