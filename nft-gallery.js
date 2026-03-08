0bc7755db6b9d40e64878b3d969d194d89059db1
MASIVO <1masivo1@gmail.com>
2026-03-07 22:25:35 -0500

    Update nft-gallery.js

--- nft-gallery.js
+++ nft-gallery.js
@@ -1,1 +1,20 @@
+const gallery = document.getElementById("nft-gallery")
 
+const totalNFT = 1000
+
+for(let i=1;i<=totalNFT;i++){
+
+const nft=document.createElement("div")
+nft.className="nft"
+
+const img=document.createElement("img")
+
+img.src=`https://raw.githubusercontent.com/1MASIVO1/MASIVO-METAVERSE/main/images/${i}.png`
+
+img.onerror=()=>nft.remove()
+
+nft.appendChild(img)
+
+gallery.appendChild(nft)
+
+}
