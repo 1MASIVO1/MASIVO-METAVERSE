27a94643b34dfdca06b0c93f98947d36374b4e5e
MASIVO <1masivo1@gmail.com>
2026-03-07 21:54:14 -0500

    Update ranking.js

--- ranking.js
+++ ranking.js
@@ -1,1 +1,25 @@
+async function cargarRanking(){
 
+const {data}=await supabase
+
+.from("stats")
+
+.select("*")
+
+.order("likes",{ascending:false})
+
+.limit(10)
+
+const div=document.getElementById("rankingLikes")
+
+div.innerHTML=""
+
+data.forEach(n=>{
+
+div.innerHTML+=`NFT ${n.nft_id} ❤️ ${n.likes}`
+
+})
+
+}
+
+cargarRanking()
