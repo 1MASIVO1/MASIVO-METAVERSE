354c03b25ecc00e5eb00e724ec0ee1b43b1d5d3b
MASIVO <1masivo1@gmail.com>
2026-03-08 15:02:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const supabaseUrl = "https://rnkuxwsuztewgbdmjyxt.supabase.co"
+
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJua3V4d3N1enRld2diZG1qeXh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5ODU4MjQsImV4cCI6MjA4NzU2MTgyNH0.mwGzWUk6xOry9BcwqwRnXGFfGMwoetg6D2pxAz7_eN4"
 
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey)
 
 
 
-// CARGAR DATOS
+// CARGAR STATS AL ABRIR
 async function cargarStats(){
 
 const { data, error } = await supabaseClient
@@ -13,8 +14,11 @@ const { data, error } = await supabaseClient
 .select("*")
 
 if(error){
+
 console.log(error)
+
 return
+
 }
 
 data.forEach(nft => {
@@ -47,6 +51,7 @@ modal.style.display = "flex"
 modalImg.src = img.src
 
 let nft = img.closest(".nft")
+
 let id = nft.dataset.id
 
 sumarView(id,nft)
@@ -78,6 +83,8 @@ await supabaseClient
 .update({ views: views })
 .eq("id", id)
 
+checkLogro(id,nft,views)
+
 }
 
 
@@ -88,6 +95,7 @@ async function like(event,btn){
 event.stopPropagation()
 
 let nft = btn.closest(".nft")
+
 let id = nft.dataset.id
 
 let span = nft.querySelector(".likes")
@@ -101,6 +109,8 @@ await supabaseClient
 .update({ likes: likes })
 .eq("id", id)
 
+checkLogro(id,nft,likes)
+
 }
 
 
@@ -111,13 +121,17 @@ async function descargar(event,btn){
 event.stopPropagation()
 
 let nft = btn.closest(".nft")
+
 let id = nft.dataset.id
 
 let img = nft.querySelector("img").src
 
 let a = document.createElement("a")
+
 a.href = img
+
 a.download = "nft.png"
+
 a.click()
 
 let span = nft.querySelector(".downloads")
@@ -131,6 +145,8 @@ await supabaseClient
 .update({ downloads: downloads })
 .eq("id", id)
 
+checkLogro(id,nft,downloads)
+
 }
 
 
@@ -141,6 +157,7 @@ async function share(event,btn){
 event.stopPropagation()
 
 let nft = btn.closest(".nft")
+
 let id = nft.dataset.id
 
 let link = window.location.origin + window.location.pathname + "?nft=" + id
@@ -159,5 +176,54 @@ await supabaseClient
 .from("nfts")
 .update({ shares: shares })
 .eq("id", id)
+
+checkLogro(id,nft,shares)
+
+}
+
+
+
+// SISTEMA DE LOGROS
+async function checkLogro(id,nft,total){
+
+if(total % 100 === 0){
+
+let logroSpan = nft.querySelector(".logroNum")
+
+let logros = parseInt(logroSpan.innerText) + 1
+
+logroSpan.innerText = logros
+
+await supabaseClient
+.from("nfts")
+.update({ logros: logros })
+.eq("id", id)
+
+}
+
+}
+
+
+
+// ABRIR NFT DESDE LINK
+window.onload = function(){
+
+let params = new URLSearchParams(window.location.search)
+
+let nftID = params.get("nft")
+
+if(nftID){
+
+let nft = document.querySelector(`.nft[data-id='${nftID}']`)
+
+if(nft){
+
+let img = nft.querySelector("img")
+
+abrirNFT(img)
+
+}
+
+}
 
 }
