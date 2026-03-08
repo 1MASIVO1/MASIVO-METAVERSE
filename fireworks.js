988470e6c439405050d77caba7846b66a53b5d34
MASIVO <1masivo1@gmail.com>
2026-03-07 21:05:33 -0500

    Update fireworks.js

--- fireworks.js
+++ fireworks.js
@@ -6,22 +6,32 @@ canvas.height=200
 
 let particles=[]
 
-function crear(){
+function crear(x,y){
 
-let x=Math.random()*canvas.width
-let y=Math.random()*200
-
-for(let i=0;i<25;i++){
+for(let i=0;i<40;i++){
 
 particles.push({
 x:x,
 y:y,
-vx:(Math.random()-0.5)*4,
-vy:(Math.random()-0.5)*4,
-life:60
+vx:(Math.random()-0.5)*6,
+vy:(Math.random()-0.5)*6,
+life:80,
+color:"hsl("+Math.random()*360+",100%,60%)"
 })
 
 }
+
+}
+
+function explosionLogo(){
+
+let logo=document.getElementById("tituloLogo")
+let rect=logo.getBoundingClientRect()
+
+let x=rect.left+rect.width/2
+let y=rect.top+rect.height/2
+
+crear(x,y)
 
 }
 
@@ -35,8 +45,8 @@ p.x+=p.vx
 p.y+=p.vy
 p.life--
 
-ctx.fillStyle="cyan"
-ctx.fillRect(p.x,p.y,2,2)
+ctx.fillStyle=p.color
+ctx.fillRect(p.x,p.y,3,3)
 
 })
 
@@ -46,7 +56,7 @@ requestAnimationFrame(animar)
 
 }
 
-setInterval(crear,2000)
+setInterval(explosionLogo,3000)
 
 animar()
 
