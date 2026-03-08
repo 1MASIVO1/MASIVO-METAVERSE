452f2c37b7a635599a4afc340d9e21098531a094
MASIVO <1masivo1@gmail.com>
2026-03-07 22:07:02 -0500

    Update metadata.js

--- api/metadata.js
+++ api/metadata.js
@@ -1,1 +1,19 @@
+export default function handler(req, res) {
 
+  const id = req.query.id;
+
+  const metadata = {
+    name: "MASIVO NFT #" + id,
+    description: "MASIVO METAVERSE NFT Collection",
+    image: "https://raw.githubusercontent.com/1MASIVO1/MASIVO-METAVERSE/main/images/" + id + ".png",
+    attributes: [
+      {
+        trait_type: "Collection",
+        value: "MASIVO GENESIS"
+      }
+    ]
+  };
+
+  res.status(200).json(metadata);
+
+}
