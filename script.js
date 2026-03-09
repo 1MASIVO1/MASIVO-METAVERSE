7a980cd6b75d69361a4a6f5400214207ef7492c2
MASIVO <1masivo1@gmail.com>
2026-03-08 20:29:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-CODIGO SCRIPT.JS:
-
-
 const supabaseUrl = "https://rnkuxwsuztewgbdmjyxt.supabase.co"
 
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJua3V4d3N1enRld2diZG1qeXh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5ODU4MjQsImV4cCI6MjA4NzU2MTgyNH0.mwGzWUk6xOry9BcwqwRnXGFfGMwoetg6D2pxAz7_eN4"
