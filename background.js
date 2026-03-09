e6a36159a7cfb7ba89f7ef51671cf606b9b71a84
MASIVO <1masivo1@gmail.com>
2026-03-09 16:11:12 -0500

    Update background.js

--- background.js
+++ background.js
@@ -22,7 +22,9 @@ stars.push({
 x:Math.random()*canvas.width,
 y:Math.random()*canvas.height,
 speed:Math.random()*0.6+0.2,
-size:Math.random()*2
+size:Math.random()*2,
+alpha:Math.random(),
+alphaSpeed:Math.random()*0.02
 
 })
 
@@ -41,7 +43,9 @@ cubes.push({
 x:Math.random()*canvas.width,
 y:Math.random()*canvas.height,
 size:Math.random()*30+20,
-vy:Math.random()*0.3+0.1
+vy:Math.random()*0.3+0.1,
+angle:Math.random()*Math.PI,
+rotation:(Math.random()-0.5)*0.01
 
 })
 
@@ -102,17 +106,56 @@ ctx.stroke()
 
 
 
+/* SUELO ESTILO VIDEOJUEGO */
+
+function drawGameFloor(){
+
+let horizon = canvas.height*0.6
+let lines = 40
+
+ctx.strokeStyle="rgba(0,255,255,0.25)"
+
+/* líneas horizontales perspectiva */
+
+for(let i=0;i<lines;i++){
+
+let y = horizon + (i*i*2)
+
+ctx.beginPath()
+ctx.moveTo(0,y)
+ctx.lineTo(canvas.width,y)
+ctx.stroke()
+
+}
+
+/* líneas verticales perspectiva */
+
+for(let x=0;x<canvas.width;x+=80){
+
+ctx.beginPath()
+ctx.moveTo(x,horizon)
+ctx.lineTo(canvas.width/2,canvas.height)
+ctx.stroke()
+
+}
+
+}
+
+
+
 /* ANIMACION */
 
 function animate(){
 
 ctx.clearRect(0,0,canvas.width,canvas.height)
 
-
-
 /* GRID */
 
 drawGrid()
+
+/* SUELO */
+
+drawGameFloor()
 
 
 
@@ -124,9 +167,12 @@ s.y+=s.speed
 
 if(s.y>canvas.height) s.y=0
 
+s.alpha+=s.alphaSpeed
+if(s.alpha>1 || s.alpha<0) s.alphaSpeed*=-1
+
 ctx.beginPath()
 ctx.arc(s.x,s.y,s.size,0,Math.PI*2)
-ctx.fillStyle="white"
+ctx.fillStyle=`rgba(255,255,255,${s.alpha})`
 ctx.fill()
 
 })
@@ -138,12 +184,18 @@ ctx.fill()
 cubes.forEach(c=>{
 
 c.y-=c.vy
+c.angle+=c.rotation
 
 if(c.y<-c.size) c.y=canvas.height+c.size
 
-ctx.strokeStyle="cyan"
+ctx.save()
+ctx.translate(c.x,c.y)
+ctx.rotate(c.angle)
 
-ctx.strokeRect(c.x,c.y,c.size,c.size)
+ctx.strokeStyle="cyan"
+ctx.strokeRect(-c.size/2,-c.size/2,c.size,c.size)
+
+ctx.restore()
 
 })
 
@@ -156,6 +208,17 @@ meteors.forEach(m=>{
 m.x+=m.vx
 m.y+=m.vy
 m.life--
+
+/* cola */
+
+ctx.beginPath()
+ctx.moveTo(m.x,m.y)
+ctx.lineTo(m.x-m.vx*8,m.y-m.vy*8)
+ctx.strokeStyle="orange"
+ctx.lineWidth=2
+ctx.stroke()
+
+/* núcleo */
 
 ctx.beginPath()
 ctx.arc(m.x,m.y,m.size,0,Math.PI*2)
